86a559ed266f99f2919c2b97428bff2d115e54ea
satanic <cflikethepsp@gmail.com>
2018-03-07 20:12:55 +0900

    packed contents in <head>
    ...and rename "sample.css" to "mytemplate.css"

---
+++ site_elements/head.js
@@ -0,0 +1,6 @@
+// 数式の設定
+$.getScript("https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-AMS-MML_HTMLorMML&locale=ja");
+$.getScript("/site_elements/MathJax.js");
+
+// サイトパーツの設定
+$.getScript("/site_elements/load_elements.js");
